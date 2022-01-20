60c16614dd5742783341a33f4367e1e1a20e1290
TobiStrings <ligalititobiloluwa2005@gmail.com>
2022-01-19 21:08:07 -0800

    TrackingInfo service

--- src/main/java/com/inventoryManagement/inventoryManagementSystem/service/TrackingInfoService/TrackingInfoService.java
+++ src/main/java/com/inventoryManagement/inventoryManagementSystem/service/TrackingInfoService/TrackingInfoService.java
@@ -2,11 +2,16 @@ package com.inventoryManagement.inventoryManagementSystem.service.TrackingInfoSe
 
 import com.inventoryManagement.inventoryManagementSystem.data.models.TrackingInfo;
 import com.inventoryManagement.inventoryManagementSystem.service.exceptions.InventoryException;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
+@Service
 public interface TrackingInfoService {
     TrackingInfo findProductInfoByName(String productName) throws InventoryException;
     TrackingInfo save(TrackingInfo trackInfo);
     List<TrackingInfo> findAllTrackingInfo() throws InventoryException;
+    void deleteProduct(String productName) throws InventoryException;
+    void writeToCsv() throws IOException;
 }
--- src/main/java/com/inventoryManagement/inventoryManagementSystem/service/TrackingInfoService/TrackingInfoServiceImpl.java
+++ src/main/java/com/inventoryManagement/inventoryManagementSystem/service/TrackingInfoService/TrackingInfoServiceImpl.java
@@ -1,21 +1,32 @@
 package com.inventoryManagement.inventoryManagementSystem.service.TrackingInfoService;
 
 import com.inventoryManagement.inventoryManagementSystem.data.models.TrackingInfo;
-import com.inventoryManagement.inventoryManagementSystem.data.repositories.TrackingInfoRepo;
+import com.inventoryManagement.inventoryManagementSystem.data.repositories.TrackingInfoRepository;
+import com.inventoryManagement.inventoryManagementSystem.service.ProductService.ProductService;
+import com.inventoryManagement.inventoryManagementSystem.service.ProductService.ProductServiceImpl;
+import com.inventoryManagement.inventoryManagementSystem.service.dtos.ProductDto;
 import com.inventoryManagement.inventoryManagementSystem.service.exceptions.InventoryException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
-import java.util.Locale;
 
 @Service
+@RequiredArgsConstructor
 public class TrackingInfoServiceImpl implements TrackingInfoService{
-    @Autowired
-    private  TrackingInfoRepo trackRepoImpl;
+
+    private final TrackingInfoRepository trackingInfoRepositoryImpl;
+
+    private final ModelMapper mapper = new ModelMapper();
+    private final ProductService productServiceImpl;
     @Override
     public TrackingInfo findProductInfoByName(String productName) throws InventoryException {
-        TrackingInfo productInfo = trackRepoImpl.findByProductName(productName.toLowerCase());
+        TrackingInfo productInfo = trackingInfoRepositoryImpl.findByProductName(productName.toLowerCase());
         if (productInfo != null){
             return productInfo;
         }
@@ -25,13 +36,37 @@ public class TrackingInfoServiceImpl implements TrackingInfoService{
     @Override
     public TrackingInfo save(TrackingInfo trackInfo) {
         trackInfo.setProductName(trackInfo.getProductName().toLowerCase());
-        return trackRepoImpl.save(trackInfo);
+        return trackingInfoRepositoryImpl.save(trackInfo);
     }
 
     @Override
     public List<TrackingInfo> findAllTrackingInfo() throws InventoryException {
-        if (!trackRepoImpl.findAll().isEmpty()) return trackRepoImpl.findAll();
+        if (!trackingInfoRepositoryImpl.findAll().isEmpty()) return trackingInfoRepositoryImpl.findAll();
         throw new InventoryException("There is no tracks being kept");
+    }
+
+    @Override
+    public void deleteProduct(String productName) throws InventoryException {
+        TrackingInfo productInfoToDelete = findProductInfoByName(productName);
+        if (productInfoToDelete == null) throw new InventoryException("Product not found");
+        ProductDto productDto = new ProductDto();
+        mapper.map(productInfoToDelete,productDto);
+        productServiceImpl.removeProduct(productDto);
+        trackingInfoRepositoryImpl.deleteByProductName(productName);
+    }
+
+    @Override
+    public void writeToCsv() throws IOException {
+        File csvOutputFile = new File("inventory.csv");
+        if(!csvOutputFile.exists()) {
+            boolean newFile = csvOutputFile.createNewFile();
+        }
+
+        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+            trackingInfoRepositoryImpl.findAll().stream()
+                    .map(TrackingInfo::toCsvRow)
+                    .forEach(pw::println);
+        }
     }
 
 
